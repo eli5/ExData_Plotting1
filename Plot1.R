d7bc0000fba0d5ac9c4fd8165a49255f7d2c9c08
eli5 <eli5@users.noreply.github.com>
2014-05-13 23:53:13 +0200

    household_power_consumption.txt read here

--- Plot1.R
+++ Plot1.R
@@ -1,3 +1,12 @@
+initial <- read.table(file="household_power_consumption.txt", header=TRUE,na.strings="?",sep=";", nrows = 100)
+classes <- sapply(initial, class)
+data <- read.table(file="household_power_consumption.txt", header=TRUE,na.strings="?",sep=";", colClasses = classes)
+
+data$Date <- as.Date(strptime(data$Date, "%d/%m/%Y") )
+df <- rbind(subset(data, data$Date == "2007-02-01"),subset(data, data$Date == "2007-02-02"))
+
+date <- paste(df$Date, df$Time); as_date  <- strptime(date, "%Y-%m-%d %H:%M:%S") 
+
 #Plot1.R
 png(filename="Plot1.png", width=480, height=480)
 hist(df$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power")
